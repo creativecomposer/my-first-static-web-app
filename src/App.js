a60987a7a7d6153d7f5bc019d724fe5a4b4931c6
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-25 15:48:47 +0200

    Make API request to backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(function () {
+    const fetchAlbums = async function () {
+      const albums = await fetch(
+        'https://album-api.kindfield-8c0c0f4f.canadacentral.azurecontainerapps.io/albums'
+      );
+      console.log(await albums.json());
+    };
+
+    fetchAlbums();
+  }, []);
+
   const value = 'Azure World';
   return <div>Hello {value}</div>;
 }
